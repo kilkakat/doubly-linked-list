cb0ba3e631af861530a682c4a72d22b735520d1a
kilkakat <kkilasoniya@gmail.com>
2024-03-15 13:08:15 +0200

    adding pushBack()

---
+++ doubleLink.js
@@ -0,0 +1,53 @@
+class DobleLinkedList {
+    constructor () {
+    this.first = null;
+    this.last = null;
+}
+
+
+
+print() {
+
+    let curr = this.first;
+
+    while (curr != null) {
+        console.log(curr.data);
+        curr = curr.next;
+    }
+}
+
+pushBack(data) {
+
+    const newNode = new Node(data);
+
+    if (!this.first) {
+        this.first = newNode;
+        this.last = newNode;
+        return;
+    }
+
+    this.last.next = newNode;
+    newNode.prev = this.last;
+    this.last = newNode;
+}
+}
+
+
+
+class Node {
+    constructor(data) {
+        this.prev = null;
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+const list = new DobleLinkedList();
+
+list.pushBack(1);
+list.pushBack(4);
+list.pushBack(2);
+list.pushBack(3); 
+
+list.print();
